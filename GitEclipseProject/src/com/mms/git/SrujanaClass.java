8c5b210e259da1dd555e2db2ba7517431dcc89d9
srujana <srujana@sujji>
2016-10-18 13:42:37 -0400

    commit again

---
+++ GitEclipseProject/src/com/mms/git/SrujanaClass.java
@@ -0,0 +1,10 @@
+package com.mms.git;
+
+public class SrujanaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("New class in srujana branch");
+	}
+
+}
